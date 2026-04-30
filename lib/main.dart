29b501ca707f2da08b05e50ab915464afac9d099
hlakhaled <hlakhaled647@gmail.com>
2026-04-30 17:45:39 +0300

    Integrate Firebase authentication and core services into the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:planova_app/core/constants/app_colors.dart';
 import 'package:planova_app/core/constants/app_router.dart';
+import 'package:planova_app/firebase_options.dart';
 import 'package:provider/provider.dart';
 import 'features/auth/providers/auth_provider.dart';
 
-void main() {
+void main()async {  
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MultiProvider(
       providers: [ChangeNotifierProvider(create: (_) => AuthProvider())],
